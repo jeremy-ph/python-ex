127be35a166ce966e20ebea554eb74d5e0e1a3ef
jeremy <bum3144@gmail.com>
2021-08-11 09:57:23 +0000

    엑셀 파일 쓰는 예 ~3

--- python_example/load_xls.py
+++ python_example/load_xls.py
@@ -5,11 +5,20 @@ import openpyxl
 wb = openpyxl.load_workbook('./sample.xlsx')
 sheet = wb['Sheet1']
 
+# # ex 1 
 # print(sheet.max_column, sheet.max_row)
 # print(sheet.cell(row=1, column=1).value)
 # print(sheet.cell(row=2, column=1).value)
 
-for row in sheet.iter_rows(min_row=2):
+# # ex 2
+# for row in sheet.iter_rows(min_row=2):
+#     for cell in row:
+#         print(cell.value)
+#     print("-" * 10)
+
+# ex 3
+cells = sheet['A2':'C4']
+for row in cells:
     for cell in row:
         print(cell.value)
     print("-" * 10)
